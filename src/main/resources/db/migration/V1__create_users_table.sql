6cea38b2045d8e967473116a927044780f881477
MostafaMahmoudegy10 <mostafa.mahmoudegy10@gmail.com>
2026-08-01 13:35:36 +0300

    application properts

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,19 +1,10 @@
-drop table users if exists;
-
-create table users(
+create table users (
     id uuid primary key,
-
     first_name varchar(255) not null,
-
     last_name varchar(255) not null,
-
     email varchar(255) not null unique,
-
     password varchar(255) not null,
-
     enabled boolean not null default false,
-
     role varchar(20) not null,
-
     creates_at timestamp not null
 );
